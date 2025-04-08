d0395647c026676f642f3d39031bf7d923c382aa
Conner <connerross@ufl.edu>
2025-04-08 13:35:30 -0400

    Began workout class and added exercise class. Keep in mind exercises attribute of workout class will have to be changed from a dict to a list after the exercise class implemtation is finished.

---
+++ Exercise Class.py
@@ -0,0 +1,6 @@
+class exercise:
+    def __init__(self, name: str, sets: int, reps: , weight):
+        self.name = name
+        self.sets = sets
+        self.reps = reps
+        self.weight = weight
